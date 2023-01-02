3064d813214484dc4ba5a76356b999cf9a795be2
Gabriel Sales <gabrielsales081@gmail.com>
2023-01-02 11:22:31 -0300

    adicionando funcao de editar e excluir tarefas

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,24 @@
-const addForm = document.querySelector("#add-form");
+const divAddForm = document.querySelector("#add-form-div");
 const addInput = document.querySelector("#add-input");
-const todoList = document.querySelector(".toDo-list");
+
+const todoList = document.querySelector(".todo-list");
+const divTodoList = document.querySelector("#todo-list-div");
+
+const divEditForm = document.querySelector("#edit-form-div");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-btn");
+
+const tollsBar = document.querySelector("#tollBar");
+
 const searchInput = document.querySelector("#search-input");
+const divSearch = document.querySelector("#search");
 const eraseBtn = document.querySelector("#erase-btn");
+
 const filterBtn = document.querySelector("#filter-select");
-const tollBar = document.querySelector("#tollBar");
+const divFilter = document.querySelector("#filter");
+
+let oldInputValue;
 
 /* Funções */
 function saveTodo(text) {
@@ -18,7 +29,7 @@ function saveTodo(text) {
   const btnCheck = document.createElement("button");
   const btnRemove = document.createElement("button");
 
-  tagLi.classList.add("toDo-item");
+  tagLi.classList.add("todo-item");
 
   divBtns.classList.add("btn-todo");
 
@@ -44,15 +55,28 @@ function saveTodo(text) {
 }
 
 function hideForms() {
-  editForm.classList.toggle("hide");
+  divEditForm.classList.toggle("hide");
 
-  addForm.classList.toggle("hide");
-  todoList.classList.toggle("hide");
-  tollBar.classList.toggle("hide");
+  divAddForm.classList.toggle("hide");
+  divTodoList.classList.toggle("hide");
+  divSearch.classList.toggle("hide");
+  divFilter.classList.toggle("hide");
+}
+
+function updateTodo(text) {
+  const todos = document.querySelectorAll(".todo-item");
+
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("p");
+
+    if (todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text;
+    }
+  });
 }
 
 /* Eventos */
-addForm.addEventListener("submit", (event) => {
+divAddForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const inputValue = addInput.value;
@@ -65,6 +89,11 @@ addForm.addEventListener("submit", (event) => {
 document.addEventListener("click", (event) => {
   const targetElement = event.target;
   const parentElement = targetElement.closest("li");
+  let todoTitle;
+
+  if (parentElement && parentElement.querySelector("p")) {
+    todoTitle = parentElement.querySelector("p").innerText || "";
+  }
 
   if (targetElement.classList.contains("finish-todo")) {
     parentElement.classList.toggle("done");
@@ -75,7 +104,27 @@ document.addEventListener("click", (event) => {
   }
 
   if (targetElement.classList.contains("edit-todo")) {
-    // console.log("editou");
     hideForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
   }
+});
+
+cancelEditBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  hideForms();
+});
+
+editForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const editInputValue = editInput.value;
+
+  if (editInputValue) {
+    updateTodo(editInputValue);
+  }
+
+  hideForms();
 });
